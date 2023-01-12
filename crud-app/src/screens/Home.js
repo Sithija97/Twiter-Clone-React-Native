76e89f7926ee956ed431097e2007cc097f92b54f
Sithija Shehara <nsithijashehara@gmail.com>
2023-01-12 23:24:22 +0530

    added data retreiving section

--- crud-app/src/screens/Home.js
+++ crud-app/src/screens/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, TouchableOpacity, Button } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { database } from '../config/firebase'
@@ -6,11 +6,32 @@ import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
 import Product from '../components/Products'
 
 const Home = () => {
-    const navigation = useNavigation()
+  const navigation = useNavigation()
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const collectionRef = collection(database, 'products');
+    const q = query(collectionRef, orderBy('creaetdAt', 'desc'));
+    const unsubscribe = onSnapshot(q, querySnapshot => {
+      setProducts(
+        querySnapshot.docs.map(doc => ({
+          id: doc.id,
+          emoji: doc.data().emoji,
+          name: doc.data().name,
+          price: doc.data().price,
+          isSold: doc.data().isSold,
+          createdAt: doc.data().createdAt
+        }))
+      )
+    })
+    return unsubscribe;
+  }, [])
+
   return (
     <View>
       <Text>Home</Text>
-      <Button title='go to Add Screen' onPress={()=>navigation.navigate("Add")}></Button>
+      {products.map(product => <Product key={product.id} {...product} />)}
+      <Button title='go to Add Screen' onPress={() => navigation.navigate("Add")}></Button>
     </View>
   )
 }
