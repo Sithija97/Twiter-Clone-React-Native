0b5b2b2c97842a93641934ac80f085a751b789fa
Sithija Shehara <nsithijashehara@gmail.com>
2023-05-28 22:35:13 +0530

    added QR reader implementation

--- QR-scanner-app/App.js
+++ QR-scanner-app/App.js
@@ -1,10 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(false);
+  const [scanData, setScanData] = useState();
+
+  const requestPermission = async () => {
+    const { status } = await BarCodeScanner.requestPermissionsAsync();
+    setHasPermission(status === "granted");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  if (!hasPermission) {
+    return (
+      <View style={styles.container}>
+        <Text>Please grant permissions to app</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  const handledBarCodeScanned = ({ type, data }) => {
+    setScanData(data);
+    console.log(`Data : ${data}`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <BarCodeScanner
+        style={StyleSheet.absoluteFillObject}
+        onBarCodeScanned={scanData ? undefined : handledBarCodeScanned}
+      />
+      {scanData && (
+        <Button title="Scan Again ?" onPress={() => setScanData(undefined)} />
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +47,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
