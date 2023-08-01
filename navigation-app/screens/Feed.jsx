68a059a9bdfef77f404e222ef7a6fd6ae568c245
Sithija Shehara <nsithijashehara@gmail.com>
2023-08-01 12:47:18 +0530

    added basic stuctures to the app

---
+++ navigation-app/screens/Feed.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+
+const Feed = () => {
+  return (
+    <SafeAreaView>
+      <Text>Feed</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Feed;
