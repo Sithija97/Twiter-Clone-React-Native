46c3353c93f68a43a4b789649542a126bd6d01ad
Sithija Shehara <nsithijashehara@gmail.com>
2023-08-05 11:27:38 +0530

    bottom tab navigation added

---
+++ navigation-app/Navigation.jsx
@@ -0,0 +1,22 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Feed from "./screens/Feed";
+
+const Tab = createBottomTabNavigator();
+
+function TabGroup() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Feed} />
+      <Tab.Screen name="Feed" component={Feed} />
+    </Tab.Navigator>
+  );
+}
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <TabGroup />
+    </NavigationContainer>
+  );
+}
