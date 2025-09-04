775d8b68a1740f663d82c523468fd0ea78dbd643
Sithija Shehara <nsithijashehara@gmail.com>
2025-09-05 00:03:30 +0530

    app config

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,33 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import userRoutes from "./routes/user.route.js";
 
 const app = express();
-const PORT = 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from ESM + TypeScript 🚀");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+app.use("/api/users", userRoutes);
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(ENV.PORT, () =>
+      console.log(`Server is up and running on PORT:${ENV.PORT} 🚀`)
+    );
+  } catch (error: any) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
