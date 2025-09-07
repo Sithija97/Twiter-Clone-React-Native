972a6813673c8daf3394f25b5b77e820d7c43da4
Sithija Shehara <nsithijashehara@gmail.com>
2025-09-07 07:06:08 +0530

    index and controllers

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,28 +2,40 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
-import { clerkMiddleware } from "@clerk/express";
 import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import { clerkMiddleware } from "@clerk/express";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from ESM + TypeScript 🚀");
 });
 
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 const startServer = async () => {
   try {
     await connectDB();
 
-    app.listen(ENV.PORT, () =>
-      console.log(`Server is up and running on PORT:${ENV.PORT} 🚀`)
-    );
+    // listen for local development
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () =>
+        console.log("Server is up and running on PORT:", ENV.PORT)
+      );
+    }
   } catch (error: any) {
     console.error("Failed to start server:", error.message);
     process.exit(1);
@@ -31,3 +43,6 @@ const startServer = async () => {
 };
 
 startServer();
+
+// export for vercel
+export default app;
